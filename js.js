aa7511f23611fff02f484d0b107eaa0996328cdc
harsha333300 <harshabsmandya@gmail.com>
2023-08-17 17:37:33 +0530

    resume

---
+++ js.js
@@ -0,0 +1,5 @@
+var clicks=document.getElementById('demo1');
+clicks.addEventListener('click',function(){
+    var countElement=document.querySelector('#mydiv > p')
+    countElement.innerHTML='harshabsmandya@gmail.com'
+})
