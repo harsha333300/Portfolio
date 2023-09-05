4870574f5cc7613ef4864448cd249a0c823bb265
harsha333300 <harshabsmandya@gmail.com>
2023-09-05 17:18:51 +0530

    completed

---
+++ Resume.js
@@ -0,0 +1,30 @@
+<div class="project-section">
+<div class="project-height">
+    <img class="project-image"
+        src="https://thumbs.dreamstime.com/b/projects-concept-black-chalkboard-d-rendering-handwritten-top-view-office-desk-lot-business-office-supplies-79906734.jpg"
+        alt="">
+    <p class="project-title">Projects!</p>
+</div>
+<div class="mini-card col-md-2 ">
+    <article class="mini-cardd ">
+        <p class="project-heading">Oberoihotels
+        </p><a href="https://www.oberoihotels.com/" target="_blank"><img
+                src="https://www.oberoihotels.com/-/media/oberoi-hotels/home-detail/madina.png?w=906&hash=44698cc5778ed1204a1221a23e5f456b"
+                alt="" class="freelance-image"></a>
+    </article>
+    <article class="mini-cardd col-md-2">
+        <p class="project-heading">Techglobal</p><a href="https://www.teachglobal.com/" target="_blank"><img
+                src="https://www.teachglobal.com/images/UNS/shivaji.jpg" alt="" class="freelance-image"></a>
+    </article>
+    <article class="mini-cardd col-md-2">
+        <p class="project-heading">Finalsite</p><a href="https://www.finalsite.com/" target="_blank"><img
+                src="https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_2/v1663250610/finalsite/bmpcfvnfqlpvr8laceh0/Press-Release-Post-Graphic.jpg"
+                alt="" class="freelance-image"></a>
+    </article>
+    <article class="mini-cardd col-md-2">
+        <p class="project-heading">Bestgift</p><a href="https://www.bestgift.in/" target="_blank"><img
+                src="https://blog.wilton.com/wp-content/uploads/sites/2/2018/05/tassel-hat-cake.jpg" alt=""
+                class="freelance-image"></a>
+    </article>
+</div>
+</div>
